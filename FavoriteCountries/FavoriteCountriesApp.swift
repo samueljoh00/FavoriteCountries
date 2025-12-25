cceb774523cc31807e877782ee347ddcf19ee8d3
Samuel Oh <samueljoh00@gmail.com>
2025-12-25 16:30:00 -0500

    Initial Commit

---
+++ FavoriteCountries/FavoriteCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteCountriesApp.swift
+//  FavoriteCountries
+//
+//  Created by Samuel Oh on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
